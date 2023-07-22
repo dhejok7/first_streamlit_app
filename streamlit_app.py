c9789b64e9690dcbecf7f010c80886c01c85f270
Dhejonithan <109928697+dhejok7@users.noreply.github.com>
2023-07-22 13:05:11 +0530

    Update streamlit_app.py
    add kiwi 
    comment json

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,8 +21,8 @@ streamlit.header("Fruityvice Fruit Advice!")
 
 #Call the Fruityvice API from Our Streamlit App
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+# streamlit.text(fruityvice_response.json())
 
 # take json and normalize
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
